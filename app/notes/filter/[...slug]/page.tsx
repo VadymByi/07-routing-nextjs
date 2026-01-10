a0550bdb6bc21429541458f91298f86a5193a769
Buinov Vadym <karabincom@gmail.com>
2026-01-10 22:42:34 +0200

    Autocheck structure3

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import NotesClient from '@/app/notes/Notes.client';
+import NotesClient from '@/app/notes/filter/[...slug]/Notes.client';
 
 export default function FilteredNotesPage() {
   return <NotesClient />;
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, dehydrate } from '@tanstack/react-query';
 import { HydrationBoundary } from '@tanstack/react-query';
-import NotesClient from './Notes.client';
+import NotesClient from './filter/[...slug]/Notes.client';
 import { fetchNotes } from '@/lib/api';
 
 const PER_PAGE = 10;
